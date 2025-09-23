de305c80d5239d7f99046de84e430c894aaad15d
Mahmoud Sadrian <83926434+mahmoud-sdr@users.noreply.github.com>
2025-09-23 21:35:26 +0330

    create secrets.example.h

---
+++ micro-bedroom_light/secrets.example.h
@@ -0,0 +1,15 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// --- WiFi Settings ---
+const char* WIFI_SSID = "WiFi-SSID";
+const char* WIFI_PASSWORD = "WiFi-Password";
+
+// --- Server Settings ---
+const char* SERVER_HOST = "example.com";
+const char* API_KEY = "********"; // Your real API key
+
+// --- Device Settings ---
+const int DEVICE_ID = 12345;
+
+#endif
